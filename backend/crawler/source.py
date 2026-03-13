61697ef68cd1f84d6e2c693b1dba2c13cf770e51
Abhishek Kori <abhishekkori601@gmail.com>
2026-03-13 19:21:22 +0530

    source.py

---
+++ backend/crawler/source.py
@@ -0,0 +1,16 @@
+EVENT_SOURCES = [
+
+    "https://www.mvp.de/heilbronn/veranstaltungen/",
+    "https://www.heilbronn.de/tourismus/veranstaltungskalender.html",
+    "https://www.heilbronn.de/kultur-freizeit/veranstaltungen.html",
+    "https://www.heilbronnerland.de/info-service/veranstaltungen",
+    "https://www.moritz.de/",
+    "https://www.eventim.de/city/heilbronn-63/tomorrow/?srsltid=AfmBOorrZGlgRVfC2SoGjDkRSAgvJYRZbxMcpNAIVLKuF8UQBUNci9_C",
+    "https://eventfrog.de/de/events/heilbronn.html",
+    "https://meine.stimme.de/event/stadtkreis-heilbronn",
+    "https://www.veranstaltung-baden-wuerttemberg.de/veranstaltungen-heilbronn-qqc100683ch30h",
+    "https://www.adticket.de/Heilbronn/PLZ-74072/Veranstaltungen/Tickets.html",
+    "https://www.stimme.de/freizeit/veranstaltungen-heilbronn/ergebnis/",
+    "https://meine.stimme.de/event/heilbronn/list"
+
+]
